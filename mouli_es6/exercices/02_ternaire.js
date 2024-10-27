29f3f97faabc56301b9f557a25d9fc65170ca7f0
Allan <allan.bernier1@gmail.com>
2024-10-27 20:25:10 +0100

    Exercices ES6

---
+++ mouli_es6/exercices/02_ternaire.js
@@ -0,0 +1,12 @@
+
+/**
+ * retourne la valeur la plus grande des deux paramètres
+ * 
+ * Contraintes:
+ *    - utiliser l'opérateur ternaire (if interdit)
+ */
+function ternaire() {}
+
+console.log(ternaire(1, 2));
+
+module.exports = ternaire;
